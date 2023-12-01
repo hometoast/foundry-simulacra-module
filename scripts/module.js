de9d189ca65532e3fc241295d390340486c401dd
hometoast <dan@hometoast.com>
2023-11-30 20:53:22 -0500

    figuring out the screen dimensions

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,23 @@
-Hooks.once('init', async function() {
+Hooks.once('init', async function () {
+    console.debug("simulacra initting!");
 
 });
 
-Hooks.once('ready', async function() {
-
+Hooks.once('ready', async function () {
+    console.debug("simulacra ready!");
 });
+
+Hooks.on('canvasPan', async function () {
+    console.debug(sceneBounds(game.canvas));
+});
+
+function sceneBounds(canvas) {
+    var sp = canvas.scene._viewPosition;
+    var sd = { w: canvas.screenDimensions[0], h: canvas.screenDimensions[1] };
+    return {
+        top: sp.y - (sd.h / 2) / sp.scale,
+        left: sp.x - (sd.w / 2) / sp.scale,
+        bottom: sp.y + (sd.h / 2) / sp.scale,
+        right: sp.x + (sd.w / 2) / sp.scale
+    };
+}
